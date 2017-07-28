8c87fc6ddf3937a99c84bdf2ea06e5a8af822845
Psycho90 <maximsh90@mail.ru>
2017-07-28 14:17:48 +0300

    Update LandingPage.js

--- LandingPage.js
+++ LandingPage.js
@@ -1,31 +1,31 @@
 function myFunction() {
     var bl = document.createElement("IMG");
     bl.setAttribute("src", "Black.png");
-    bl.setAttribute("height", "600vh");
+    bl.setAttribute("height", "100%");
     bl.setAttribute("alt", "Black Silhouette");
     bl.classList.add ("blackImg");
-    document.body.appendChild(bl);
+    document.getElementById ("imgWrapper").appendChild(bl);
 
     var f = document.createElement("IMG");
     f.setAttribute("src", "fire.png");
-    f.setAttribute("height", "600vh");
+    f.setAttribute("height", "100%");
     f.setAttribute("alt", "Fire Silhouette");
     f.classList.add ("blackImg", "fireImg");
-	document.body.appendChild(f);
+	document.getElementById ("imgWrapper").appendChild(f);
 
     var s = document.createElement("IMG");
     s.setAttribute("src", "Storm.png");
-    s.setAttribute("height", "600vh");
+    s.setAttribute("height", "100%");
     s.setAttribute("alt", "Storm Silhouette");
     s.classList.add ("blackImg", "stormImg");
-    document.body.appendChild(s);
+    document.getElementById ("imgWrapper").appendChild(s);
 
     var m = document.createElement("IMG");
     m.setAttribute("src", "Main.png");
-    m.setAttribute("height", "600vh");
+    m.setAttribute("height", "100%");
     m.setAttribute("alt", "Main");
     m.classList.add ("blackImg", "mainImg");
-    document.body.appendChild(m);
+    document.getElementById ("imgWrapper").appendChild(m);
 
 };
 
