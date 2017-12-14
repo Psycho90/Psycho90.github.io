316437112b24e9d363079b5f63c5eb1d5b010b07
Psycho90 <maximsh90@mail.ru>
2017-12-14 15:03:41 +0200

    Add files via upload

---
+++ src/js/script.js
@@ -0,0 +1,41 @@
+$(".intro").typeIt({
+	strings: ['Hi, I’m Max, I build websites.', 'And that\'s all you need to know.'],
+	speed: 100,
+	lifeLike: true,
+	breakDelay: 2000,
+})
+
+var $btn = $('#btn');
+
+setTimeout(function(){
+	$btn.fadeIn(1000)}, 9000);
+
+var $portfolio = $('.portfolio-item');
+
+$portfolio.mouseenter(function(){
+	var $text = $(this).find('p');
+	$text.fadeToggle(1000);
+})
+$portfolio.mouseleave(function(){
+	var $text = $(this).find('p');
+	$text.fadeToggle(1000);
+})
+
+$(window).on('scroll', function(){
+	var $wHeight = $(window).height();
+	var $wTop = $(window).scrollTop();
+	var $wBot = ($wHeight + $wTop);
+	var $skills = $('.skills>.wrapper');
+	var $skillsHeight = $skills.outerHeight();
+	var $skillsTop = $skills.offset().top;
+	var $skillsBot = ($skillsHeight + $skillsTop);
+	console.log($skillsTop, $skillsBot)
+	if(($skillsBot >= $wTop) && ($skillsTop <= $wBot)){
+		$('.skills-item').addClass('skills-item-inView')
+	}
+	else{
+		$('.skills-item').removeClass('skills-item-inView')
+	}
+})
+
+
