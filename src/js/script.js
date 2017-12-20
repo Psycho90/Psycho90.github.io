ae8e9e1ef639abac0b30d3eff9f93321eb3c5389
Psycho90 <maximsh90@mail.ru>
2017-12-20 11:19:18 +0200

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(window).on('load', (function(){
 	/*----------------MAIN TYPEWRITER EFFECT------------*/
 	$(".intro").typeIt({
 		strings: ['And that\'s all you need to know.'],
@@ -11,37 +11,54 @@ $(document).ready(function(){
 
 	setTimeout(function(){
 		$btn.fadeIn(1000)}, 3000);
-})
 
-/*-------------PORTFOLIO EFFECTS---------*/
-var $portfolio = $('.portfolio-item');
+	$btn.click(function() {
+		$('html, body').animate({
+			scrollTop: $(".contacts").eq(0).offset().top
+		}, 1000);
+	});
 
-$portfolio.mouseenter(function(){
-	var $text = $(this).find('p');
-	$text.fadeToggle(1000);
-})
-$portfolio.mouseleave(function(){
-	var $text = $(this).find('p');
-	$text.fadeToggle(1000);
-})
-/*----------SKILLS EFFECTS----------*/
-$(window).on('scroll', function(){
-	var $wHeight = $(window).height();
-	var $wTop = $(window).scrollTop();
-	var $wBot = ($wHeight + $wTop);
-	var $skills = $('.skills>.wrapper');
-	var $skillsHeight = $skills.outerHeight();
-	var $skillsTop = $skills.offset().top;
-	var $skillsBot = ($skillsHeight + $skillsTop);
+	/*-------------PORTFOLIO EFFECTS---------*/
+	var $portfolio = $('.portfolio-item');
 
+	$portfolio.mouseenter(function(){
+		var $text = $(this).find('p');
+		$text.fadeToggle(1000);
+	})
+	$portfolio.mouseleave(function(){
+		var $text = $(this).find('p');
+		$text.fadeToggle(1000);
+	})
+	/*----------SKILLS EFFECTS----------*/
+	$(window).on('scroll', function(){
+		var $wHeight = $(window).height();
+		var $wTop = $(window).scrollTop();
+		var $wBot = ($wHeight + $wTop);
+		var $skills = $('.skills>.wrapper');
+		var $skillsHeight = $skills.outerHeight();
+		var $skillsTop = $skills.offset().top;
+		var $skillsBot = ($skillsHeight + $skillsTop);
+
+	// if (($skillsBot >= $wTop) && ($skillsTop <= $wBot)){
+	// 	$('.skills-item').each(function(){
+	// 		$(this).removeClass('skills-item-anim');
+	// 	});
+	// }
+	// else {
+	// 	$('.skills-item').each(function(){
+	// 		$(this).addClass('skills-item-anim');
+	// 	});
+	// }
 	if (($skillsBot >= $wTop) && ($skillsTop <= $wBot)){
-		$('.skills-item').each(function(){
-			$(this).removeClass('skills-item-anim');
-		});
+		$('.skills-item').eq(0).removeClass('skills-item-anim');
+		$('.skills-item').eq(1).removeClass('skills-item-anim');
+		$('.skills-item').eq(2).removeClass('skills-item-anim');
 	}
-	else{
-		$('.skills-item').each(function(){
-			$(this).addClass('skills-item-anim');
-		});
+	else {
+		$('.skills-item').eq(0).addClass('skills-item-anim');
+		$('.skills-item').eq(1).addClass('skills-item-anim');
+		$('.skills-item').eq(2).addClass('skills-item-anim');
 	}
 })
+})
+)
